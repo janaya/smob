bf25b9fd4c27da27bf9076c3a7e504f7dea7020f
janaya <julia.anaya@gmail.com>
2011-07-29 00:29:37 +0100

    Adding several interests in profile

--- js/private_profile.js
+++ js/private_profile.js
@@ -1,3 +1,29 @@
+function suggestion(domform, domterm, domuri) {
+  var term = $(domterm).val();
+  var loc = document.location.href;
+  loc = loc.replace("private/edit","");
+  console.debug(loc + "ajax/suggestions.php?");
+  $.get(loc+"ajax/suggestions.php?type=tag&term="+urlencode(term)+getCacheBusterParam(), function(data){
+    console.debug(data);
+    var obj = JSON.parse(data);
+    $(domform).append(data);
+    $(domform).append(obj.html);
+  });
+}
+
+function set_suggestion_popup(domterm, domform, dombutton, domformbutton) {
+  var term = $(domterm).val();
+  var loc = document.location.href;
+  loc = loc.replace("private/edit","");
+  console.debug(loc + "ajax/suggestions.php?");
+  $.get(loc+"ajax/suggestions.php?type=tag&term="+urlencode(term)+getCacheBusterParam(), function(data){
+    console.debug(data);
+    $(domform).append(data);
+  });
+  $(dombutton).remove();
+  $(domformbutton).show();
+}
+
 function set_rel_types(domid) {
   var option_data =  "<option value='' selected=''></option>";
   $(domid).append(option_data);
@@ -6,7 +32,7 @@ function set_rel_types(domid) {
       if (rel.indexOf("http://purl.org/vocab/relationship/") === 0) {
         if (data[rel].hasOwnProperty("http://www.w3.org/2000/01/rdf-schema#label")) {
           var label = data[rel]['http://www.w3.org/2000/01/rdf-schema#label'][0]['value'];
-          var option_data =  "<option value='"+rel+"' >"+label+ "</option>";
+          var option_data =  "<option name='" + label + "' value='"+rel+"' >"+label+ "</option>";
           $(domid).append(option_data);
         }
       }
@@ -16,7 +42,7 @@ function set_rel_types(domid) {
 
 //function set_rel_types(domid, rel_types) {
 //  for (label in rel_types) {
-//    var option_data =  "<option value='"+rel_types[label]+"' >"+label+ "</option>";
+//    var option_data =  "<option value='"+label+ "' value='"+rel_types[label]+"' >"+label+ "</option>";
 //    $(domid).append(option_data);
 //  }
 //}
@@ -36,9 +62,42 @@ function set_rel_types(domid) {
 //  });
 //};
 
+function post_data2triples(user_uri) {
+  //var rel_names = [];
+ // var rel_persons = [];
+  var rel_counter = parseInt($('#rel_counter').val());
+  for(i=0; i<rel_counter; i++) {
+    var person = $('#person'+i).val();
+    var rel_type = $('#rel_type'+i).val();
+    var rel_label = $('#rel_type'+i+' option:selected').text();
+    //rel_persons[rel_type] = person;
+    //rel_names[rel_type] = rel_label;
+    triples = triples + "<" + user_uri + "> <" + rel_type + "> <" + person + "> . ";
+    triples = triples + "<" + rel_type + "> <http://www.w3.org/2000/01/rdf-schema#label> <" + rel_label + "> . ";
+  }
+  //$.each(rel_persons, function(rel_type, person) { 
+  var interest_counter = parseInt($('#interest_counter').val());
+  for(i=0; i<interest_counter; i++) {
+    var interest = $('#interest'+i).val();
+    var interest_label = $('#interest'+i).attr('name');
+    triples = triples + "<" + user_uri + "> <http://xmlns.com/foaf/0.1/topic_interest> <" + interest + "> . ";
+    triples = triples + "<" + interest + "> <http://www.w3.org/2000/01/rdf-schema#label> <" + interest_label + "> . ";
+  }
+  
+  $("#privacy_result").text(triples).html();
+  var loc = document.location.href;
+  loc = loc.replace("private/edit","");
+  console.debug(loc + "ajax/private.php?" + $.param({"triples":triples}));
+  $.post(loc + "ajax/private.php?", {triples:triples}, function(data){
+    console.debug(data);
+    $("#result").html(data);
+  });
+}
+
 function post_private_profile(user_uri) {
   var persons = [];
   var rel_types = [];
+  var triples = "";
   $.each($('#private_form').serializeArray(), function(i, field) {
       var p = field.name.replace( "person", "");
       if (p.length < 3 && p.length > 0) {
@@ -60,8 +119,8 @@ function post_private_profile(user_uri) {
   console.debug(triples);
   
   $("#lod_interest :checked").each(function() {
-    lod_uri = $(this).val().split('--')[1];
-    lod_label = $(this).val().split('--')[2];
+    lod_uri = $(this).val().split('--')[2];
+    lod_label = $(this).val().split('--')[1];
     triples = triples + "<" + user_uri + ">  <http://xmlns.com/foaf/0.1/topic_interest> <" + lod_uri + "> . ";
     triples = triples + "<" + lod_uri + "> <http://www.w3.org/2000/01/rdf-schema#label> <" + lod_label + "> . ";
   })
@@ -77,15 +136,21 @@ function post_private_profile(user_uri) {
   });
 }
 
-function delRel(id) {
-  $(id).remove();
+function del(domid) {
+  $(domid).remove();
 }
 function addRel() {
-  var counter = parseInt(document.getElementById("counter").value);
-  $("#rel_block").append("<fieldset id='rel_fieldset" + counter + "'><legend>Relationship</legend>           <select id='rel_type" + counter + "' name='rel_type" + counter + "' class='required'>           </select>           <input name='person" + counter + "' id='person" + counter + "' type='text' class='url required' size='30' />           <a id='del_rel" + counter + "' href='' onClick='delRel(\"#rel_fieldset" + counter + "\"); return false;'>[-]</a>        </fieldset>");
+  var counter = parseInt($('#rel_counter').val());
+  $("#rel_block").append("<fieldset id='rel_fieldset" + counter + "'><legend>Relationship</legend>           <select id='rel_type" + counter + "' name='rel_type" + counter + "' class='required'>           </select>           <input name='person" + counter + "' id='person" + counter + "' type='text' class='url required' size='30' />           <a id='del_rel" + counter + "' href='' onClick='del(\"#rel_fieldset" + counter + "\"); return false;'>[-]</a>        </fieldset>");
   $('#rel_type').children().clone().appendTo('#rel_type' + counter);
   counter = counter + 1;
-  document.getElementById("counter").value = counter;
+  $('#rel_counter').val() = counter;
+}
+function addInterest() {
+  var counter = parseInt($('#interest_counter').val());
+  $("#interest_block").append("<fieldset id='interest_fieldset" + counter + "'><legend>interest</legend>          <input type='text' id='interes_label" + counter + "' name='interest_label" + counter + "' class='required' size='30' />           <a id='interest_suggestion" + counter + "' href='' onClick='suggestion(\"#interest_form" + counter + "\", \"#interest_label" + counter + "\", \"#interest" + counter + "\"); return false;'>Validate!</a>                      (<input name='interest" + counter + "' id='interest" + counter + "' type='text' class='url required' size='30' readonly />)           <a id='del_interest" + counter + "' href='' onClick='del(\"#interest_fieldset" + counter + "\"); return false;'>[-]</a>           <div id='interest_form" + counter + "' style='display: none;'></div>        </fieldset>");
+  counter = counter + 1;
+  parseInt($('#interest_counter').val());
 }
 
 //////////////////////////////////////////////////////////////////////////////
--- lib/smob/private_profile_template.php
+++ lib/smob/private_profile_template.php
@@ -7,16 +7,26 @@
     //var rel_types = get_rel_types();
     //console.debug(rel_types);
 
+
+  //$(function() {
+    //$('#interest_suggestion').dialog({ 
+    //    autoOpen: false,
+    //    height: 300,
+    //    width: 350,
+    //    modal: true,
+    //    buttons: {
+    //        "Ok": function() {
+    //          $("#i").val() =  $("#suggestions :checked").val();
+    //          $(this).dialog("close"); 
+    //        }
+    //    },
+    //    close: function() {
+    //      
+    //    }
+    //});
+  //});
+
     $(document).ready(function(){
-      var settings = {
-        'endpoint' : 'http://spotlight.dbpedia.org/dev/rest',
-        'confidence' : 0.2,
-        'support' : 20,
-        'powered_by': 'yes',
-        'showScores': 'yes'
-      }
-      //$('#test').runDBpediaSpotlight("candidates");
-      $('#test').annotate();
       
       $("#tabs_interest").tabs();
       $('#interest').focus(function() {
@@ -27,16 +37,15 @@
         interlink_interest('#interest', '#lod_interest', '#tabs_interest');
       });
 
-      set_rel_types('#rel_type');
+      //set_rel_types('#rel_type');
 
       $('#add_rel').click(function(e) {
         e.preventDefault();
         addRel();
       });
-
-      $('#del_rel').click(function(e) {
+      $('#add_interest').click(function(e) {
         e.preventDefault();
-        $('#rel_fieldset').remove();
+        addInterest();
       });
 
       $('#private_submit').click(function(e) {
@@ -52,7 +61,8 @@
         //$("#private_form").validate();
         var user_uri = document.location.href;
         //user_uri.replace("/private","");
-        post_private_profile(user_uri);
+        //post_private_profile("https://localhost/smob/me/private");
+        post_private_profile("https://localhost/smob/me");
       });
 
     });
@@ -64,37 +74,51 @@
   
 <h2>Private profile</h2>
     <form id="private_form">
-      <input type="text" id="test" value="semantic">
+
       </br><b>Interests</b>
-      
+
       <fieldset id="interest_fieldset"><legend>Interest</legend> 
-      <textarea id="interest" name="interest" rows="1" cols="30"><?=$params['lod'];?></textarea> 
-      <div class="interest-details" style="display: none;"> 
-      <div id="lod_interest">Links will be suggested while typing ... (space required after each #tag)
-        <div id="tabs_interest"><ul></ul></div> 
-      </div> 
+        <textarea id="interest" name="interest" rows="1" cols="30"><?=$params['lod'];?></textarea> 
+        <div class="interest-details" style="display: none;"> 
+          <div id="lod_interest">Links will be suggested while typing ... (space required after each #tag)
+            <div id="tabs_interest">
+              <ul></ul>
+            </div> 
+          </div> 
+        </div> 
       </fieldset> 
-      
+
+      <div id="interest_block">
+        <?php foreach($params['interest_fieldsets'] as $fieldset): ?>
+          <?=$fieldset;?>
+        <?php endforeach; ?>
+      </div> 
+      <p><a id="add_interest" href="">[+]</a></p>
+
+      <input type="hidden" id="interest_counter" value="<?=$params['interest_counter'];?>">
+
       </br><b>relationships</b>
-      
-      <select id="rel_type" name="rel_type" style="visibility:hidden;"></select>
-      
+
+      <select id="rel_type" name="rel_type" style="visibility:hidden;">
+          <?=$params['rel_type_options'];?>
+      </select>
+
       <div id="rel_block">
-        <?php foreach($params['fieldsets'] as $fieldset): ?>
+        <?php foreach($params['rel_fieldsets'] as $fieldset): ?>
           <?=$fieldset;?>
         <?php endforeach; ?>
       </div> 
 
       <p><a id="add_rel" href="">[+]</a></p>
        
-      <input type="hidden" id="counter" value="<?=$params['fieldsetcounter'];?>">
+      <input type="hidden" id="rel_counter" value="<?=$params['rel_counter'];?>">
        
       <button id="private_submit" class="content-details">Save</button>
     </form> 
  
 <h2>Result</h2>
     <div id="result"></div>
-    <div id="privacy_result"></div>
+    <div id="privacy_result" class="post external"></div>
  
 </div> 
  
