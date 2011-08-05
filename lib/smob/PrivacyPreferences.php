cc86ae522828fbdf3e8266991deab500dcfe3eb2
janaya <julia.anaya@gmail.com>
2011-08-05 18:05:12 +0100

    more rules

--- lib/smob/PrivacyPreferences.php
+++ lib/smob/PrivacyPreferences.php
@@ -45,35 +45,6 @@ class PrivacyPreferences {
     return $options;
   }
 
-  function get_interests($user_uri) {
-    // cleaning from previous code errors
-    $query = "DELETE FROM <$user_uri> {
-      <$user_uri> foaf:topic_interest <http://localhost:443/smob/ajax/undefined> .
-      <http://localhost:443/smob/ajax/undefined> rdfs:label ?label . 
-    } WHERE {
-      <$user_uri> foaf:topic_interest <http://localhost:443/smob/ajax/undefined> .
-      <http://localhost:443/smob/ajax/undefined> rdfs:label ?label . 
-    }";
-    //$query = "DELETE {
-    //  <$user_uri> foaf:topic_interest <http://localhost:443/smob/ajax/undefined> .
-    //  <http://localhost:443/smob/ajax/undefined> rdfs:label 'null' . 
-    //}";
-    $data = SMOBStore::query($query);
-    $interests = array();
-    $query = "SELECT ?interest ?interest_label FROM <$user_uri> WHERE {
-      <$user_uri> foaf:topic_interest ?interest .
-      ?interest rdfs:label ?interest_label . }";
-    $data = SMOBStore::query($query);
-    error_log("interests queried",0);
-    error_log(print_r($data, 1),0);
-    if($data) {
-      foreach($data as $t) {
-        $interests[$t['interest_label']] = $t['interest'];
-      }
-    };
-    return $interests;
-  }
-
   function get_relationships($user_uri) {
     $rel_persons = array();
     //"<" + user_uri + "> <" + rel_types[i] + "> <" + persons[i] + "> . ";
@@ -100,81 +71,29 @@ class PrivacyPreferences {
     return $rel_persons;
   }
 
-  function get_initial_private_form_data() {
-    $user_uri = SMOB_ROOT."me";
-    $rel_fieldsets = array();
-    $rel_persons = PrivateProfile::get_relationships($user_uri);
-    error_log(print_r($rel_persons, 1), 0);
-    $rel_types = PrivateProfile::get_rel_types();
-    $rel_type_options = PrivateProfile::set_rel_type_options($rel_types);
-    $index = 0;
-    foreach($rel_persons as $rel_type=>$person) {
-      $rel_fieldset = "
-        <div id='rel_fieldset$index'>
-           <select id='rel_type$index' name='rel_type$index' class='required'>";
-      foreach($rel_types as $label=>$rel) {
-        if($rel_type==$rel) {
-          $option = "             <option name='$label' value='$rel' selected='$rel'>$label</option>";
-        } else {
-          $option = "             <option name='$label' value='$rel' >$label</option>";
-        }
-        $rel_fieldset .= $option;
-      };
-      $rel_fieldset .= "
-           </select>
-           <input type='text' name='person$index' id='person$index' value='$person' class='url required' size='50' />
-           <a id='del_rel$index' href='' onClick='del(\"#rel_fieldset$index\"); return false;'>[-]</a>
-        </div>
-        </br>";
-      $rel_fieldsets[$index] = $rel_fieldset;
-      $index++;
-    }
-    error_log(print_r($rel_fieldsets, 1), 0);
-    error_log($index);
-    $rel_counter = $index;
-    $interest_fieldsets = array();
-    $interests = PrivateProfile::get_interests($user_uri);
-    $index = 0;
-    error_log("interests", 0);
-    foreach($interests as $interest_label=>$interest) {
-      error_log($interest, 0);
-      error_log($interest_label,0);
-      $interest_fieldset = "
-        <div id='interest_fieldset$index'>
-          <input type='text' id='interest_label$index' name='interest_label$index' value='$interest_label' class='url required' size='20' readonly />
-          (<input type='text' id='interest$index' name='interest$index' value='$interest' class='url required' size='50' readonly />)
-          <a id='del_rel$index' href='' onClick='del(\"#interest_fieldset$index\"); return false;'>[-]</a>
-        </div>
-        </br>";
-      $interest_fieldsets[$index] = $interest_fieldset;
-      $index++;
-    }
-    error_log(print_r($interest_fieldsets, 1), 0);
-    error_log($index);
-    $interest_counter = $index;
-    $params = array("rel_type_options"=>$rel_type_options,
-                    "rel_fieldsets"=>$rel_fieldsets,
-                    "rel_counter"=>$rel_counter,
-                    "interest_fieldsets"=>$interest_fieldsets,
-                    "interest_counter"=>$interest_counter
-                    );
-    return $params;
-  }
-  
-  function get_privacy_preferences() {
+  function get_interests() {
   $graph = SMOB_ROOT."ppo";
   $ppo = SMOB_ROOT."preferences";
   $resource = "http://dbpedia.org/resource/Resource\_Description\_Framework";
   $interest = "http://dbpedia.org/resource/Semantic_Web";
-  $accessspace = "?user foaf:topic_interest <$interest>";
+  $accessspace = "?user foaf:topic_interest <$interest>.
+  foaf:knows
+  foaf:intent
+  filter
+  ";
   // ?topic dcterms:subject category:Semantic_Web
   $accessquery = "SELECT ?user WHERE { $accessspace .}";
-  $triples = "
+  
+  
+  $triples = " INSERT
   <$ppo> a ppo:PrivacyPreference;
           ppo:appliesToResource <http://rdfs.org/sioc/ns#MicroblogPost>;
           ppo:hasCondition [
                             ppo:hasProperty tag:Tag;
                             ppo:resourceAsObject <$resource>
+                            ppo:resourceAsObject <RDF>
+                            ppo:resourceAsObject <db./semweb>
+                            filter?
                           ];
           ppo:assignAccess acl:Read;
           ppo:hasAccessSpace [
@@ -182,8 +101,10 @@ class PrivacyPreferences {
                               ] . ";
 
 
+
+
     $query = "INSERT INTO <$graph> { $triples }";
-    $query = "SELECT * FROM <$graph> WHERE {
+    $query = "SELECT ?accessquery FROM <$graph> WHERE {
       <$ppo> ppo:hasAccessSpace [ ppo:hasAccessQuery ?accessquery ] .
     }";
     //FILTER(REGEX(?rel_type, 'http://purl.org/vocab/relationship/', 'i')).
@@ -195,18 +116,20 @@ class PrivacyPreferences {
     if($data) {
       foreach($data as $t) {
         //$interests[$t['interest_label']] = $t['accessquery'];
-        $accessquery = 
+        $accessquery = $t['accessquery'];
       }
     };
     //return $interests;
+    return array($accessquery);
   }
 
   function get_initial_private_form_data() {
     $rel_types = PrivateProfile::get_rel_types();
     $rel_type_options = PrivateProfile::set_rel_type_options($rel_types);
+    $interest_fieldsets = PrivacyPreferences::get_interests();
     $params = array("rel_type_options"=>$rel_type_options,
-                    "rel_fieldsets"=>$rel_fieldsets,
-                    "rel_counter"=>$rel_counter,
+                    "rel_fieldsets"=>array(),
+                    "rel_counter"=>0,
                     "interest_fieldsets"=>$interest_fieldsets,
                     "interest_counter"=>0
                     );
@@ -217,8 +140,8 @@ class PrivacyPreferences {
     $file = 'privacy_preferences_template.php';
     // if(IS_AJAX) {
     // } else {
-    //$params = PrivatePreferences::get_initial_private_form_data();
-    //extract($params);
+    $params = PrivacyPreferences::get_initial_private_form_data();
+    extract($params);
     ob_start();
     include($file);
     $contents = ob_get_contents();
